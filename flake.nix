149f3376e145752e45d7d6fea5e69a11a848ccfc
seba <email@giglis.eu>
2025-06-04 23:12:08 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,29 @@
 {
-  description = "Dotfiles for use in flake inputs";
+  outputs = { self }: {
+    # expose the file as a derivation
+    zshtheme = builtins.path {
+      name = "mytheme.zsh-theme";
+      path = ./mytheme.zsh-theme;
+    };
 
-  outputs = { self }: {};
+    myfunctions = builtins.path {
+      name = "my_functions";
+      path = ./my_functions;
+    };
+
+    rioconfig = builtins.path {
+      name = "rioconfig";
+      path = ./rio/config.toml;
+    };
+
+    riotheme = builtins.path {
+      name = "riotheme";
+      path = ./rio/rose-pine.toml;
+    };
+
+    zshrc = builtins.path {
+      name = "zshrc";
+      path = ./zshrc;
+    };
+  };
 }

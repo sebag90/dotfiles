65b715d78a04578dc684d9c6289b1e45f825b367
seba <email@giglis.eu>
2025-06-18 21:09:25 +0200

    indentation

--- nix/hosts/common_packages.nix
+++ nix/hosts/common_packages.nix
@@ -2,36 +2,36 @@
 {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-      # utils
-      wget
-      curl
-      git
-      bat
-      eza
-      glow
-      btop
-      glow
+    # utils
+    wget
+    curl
+    git
+    bat
+    eza
+    glow
+    btop
+    glow
 
-      # programming
-      helix
-      rio
-      vscodium
-      just
-      uv
-      zellij
-      pre-commit
-      ruff
-      python313
-      python313Packages.pip
-      python313Packages.ipython
-      python313Packages.python-lsp-server
-      python313Packages.jedi-language-server
+    # programming
+    helix
+    rio
+    vscodium
+    just
+    uv
+    zellij
+    pre-commit
+    ruff
+    python313
+    python313Packages.pip
+    python313Packages.ipython
+    python313Packages.python-lsp-server
+    python313Packages.jedi-language-server
 
-      # containers
-      podman
-      podman-tui
-      dive
-      devcontainer
-      dockerfile-language-server-nodejs
-    ];
+    # containers
+    podman
+    podman-tui
+    dive
+    devcontainer
+    dockerfile-language-server-nodejs
+  ];
 }

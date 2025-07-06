7776f1034324c0f902833c50ecfdaef95d8522ff
seba <email@giglis.eu>
2025-07-06 20:53:22 +0200

    add fwupd for updates

--- nix/hosts/nixos/system/network.nix
+++ nix/hosts/nixos/system/network.nix
@@ -1,5 +1,5 @@
 {
-    networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary

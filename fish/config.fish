cb732e21641286b71843115ecff5fdf842afa30d
seba <email@giglis.eu>
2025-11-23 11:18:23 +0100

    update config

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,12 @@ end
 
 set -x EDITOR hx
 set -x VISUAL hx
+set -x PIP_REQUIRE_VIRTUALENV=true
 
 alias ls="eza"
 alias cat="bat"
 
 set -U fish_user_paths $HOME/go/bin $fish_user_paths
+set -U fish_user_paths $HOME/.cache/npm/global/bin $fish_user_paths
+
 fish_config theme choose rose-pine

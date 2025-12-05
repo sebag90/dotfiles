a2aebcc261d3283a8ed1033b55a64f8aaf1493b7
seba <email@giglis.eu>
2025-12-05 16:14:17 +0100

    fix path

--- fish/configs/path.fish
+++ fish/configs/path.fish
@@ -9,7 +9,7 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 
 # nix
-fish_add_path $HOME/.nix-profile/bin/hx
+fish_add_path $HOME/.nix-profile/bin
 fish_add_path /run/wrappers/bin
 fish_add_path /etc/profiles/per-user/sebastiano/bin
 fish_add_path /run/current-system/sw/bin

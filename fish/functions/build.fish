b9c00f9ae3f14f5d3aa5b5b93b78e8b02536bf9d
seba <dummy>
2026-02-01 00:12:35 +0100

    fix typo

--- fish/functions/build.fish
+++ fish/functions/build.fish
@@ -5,10 +5,10 @@ function build
     end
 
     # generic host: run nix
-    if if contains "$NIX_HOSTNAME" generic laptop
-            nix run $DOTFILES_DIR/nix#homeConfigurations.generic.activationPackage
-        else
-            # other hosts: run nix build with sudo
-            sudo $NIX_BUILD_EXEC switch --impure --flake $DOTFILES_DIR/nix#$NIX_HOSTNAME
-        end
+    if contains "$NIX_HOSTNAME" generic laptop
+        nix run $DOTFILES_DIR/nix#homeConfigurations.generic.activationPackage
+    else
+        # other hosts: run nix build with sudo
+        sudo $NIX_BUILD_EXEC switch --impure --flake $DOTFILES_DIR/nix#$NIX_HOSTNAME
     end
+end

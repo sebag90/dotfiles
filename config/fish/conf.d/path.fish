cb9e7d99d00e6ea4ba92729191b18e44936856bb
seba <email@giglis.eu>
2026-04-27 20:44:38 +0200

    update helix config (hidden files)

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,17 +1,13 @@
 # go
-fish_add_path $HOME/go/bin
+fish_add_path -a $HOME/go/bin
 
 # npm
-fish_add_path $HOME/.cache/npm/global/bin
+fish_add_path -a $HOME/.cache/npm/global/bin
 
 # rust
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.local/bin
+fish_add_path -a $HOME/.cargo/bin
+fish_add_path -a $HOME/.local/bin
 
 # nix
-fish_add_path /nix/profile/bin
-fish_add_path $HOME/.nix-profile/bin
-fish_add_path /run/wrappers/bin
-fish_add_path /etc/profiles/per-user/sebastiano/bin
-fish_add_path /run/current-system/sw/bin
-fish_add_path /nix/var/nix/profiles/default/bin
+fish_add_path -a /nix/profile/bin
+fish_add_path -a $HOME/.nix-profile/bin

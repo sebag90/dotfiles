0f1c1647a10ba0f9332108beec5190fba5ae00c2
sebastiano <sebastiano.gigliobianco@maxqda.de>
2026-08-19 11:44:11 +0000

    add mounts if they exist

--- config/fish/functions/devc.fish
+++ config/fish/functions/devc.fish
@@ -1,4 +1,17 @@
 function devc
-    podman run -it --rm --userns=keep-id --user $(id -u):$(id -g) -v $(pwd):/workspace:Z -w /workspace -v $XDG_RUNTIME_DIR/podman/podman.sock:/run/podman/podman.sock -e CONTAINER_HOST=unix:///run/podman/podman.sock --security-opt label=disable ghcr.io/sebag90/devenv:latest
-    return
+    set -l image $argv[1]
+    test -z "$image" && set image ghcr.io/sebag90/devenv:latest
+    set -l mounts
+    for d in .pi/agent .aws .gitconfig
+        test -e ~/$d && set -a mounts -v ~/$d:/home/dev/$d:Z
+    end
+    podman run -it --rm \
+        --userns=keep-id --user $(id -u):$(id -g) \
+        -w /workspace \
+        -v $(pwd):/workspace:Z \
+        -v $XDG_RUNTIME_DIR/podman/podman.sock:/run/podman/podman.sock \
+        -e CONTAINER_HOST=unix:///run/podman/podman.sock \
+        $mounts \
+        --security-opt label=disable \
+        $image
 end

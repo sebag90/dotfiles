1c7be91571783fadf5659db1126e926b831a1b95
sebastiano <sebastiano.gigliobianco@maxqda.de>
2026-08-20 06:02:23 +0000

    add netrc and ssh option

--- config/fish/functions/devc.fish
+++ config/fish/functions/devc.fish
@@ -2,11 +2,12 @@ function devc
     set -l image $argv[1]
     test -z "$image" && set image ghcr.io/sebag90/devenv:latest
     set -l mounts
-    for d in .pi/agent .aws .gitconfig .ssh
+    for d in .pi/agent .aws .gitconfig .ssh .netrc
         test -e ~/$d && set -a mounts -v ~/$d:/home/dev/$d:Z
     end
     podman run -it --rm \
         --userns=keep-id --user $(id -u):$(id -g) \
+        --passwd-entry 'dev:*:$UID:$GID::/home/dev:/nix/profile/bin/fish' \
         -w /workspace \
         -v $(pwd):/workspace:Z \
         -v $XDG_RUNTIME_DIR/podman/podman.sock:/run/podman/podman.sock \

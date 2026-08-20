bcac42f6cf86f5917c96a0a2b1297fdd5d7492d7
sebastiano <sebastiano.gigliobianco@maxqda.de>
2026-08-20 11:10:41 +0000

    remove nix and /usr/bin since already there

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,4 +1,3 @@
-fish_add_path -m /usr/bin
 # go
 fish_add_path -a $HOME/go/bin
 
@@ -8,11 +7,3 @@ fish_add_path -a $HOME/.cache/npm/global/bin
 # rust
 fish_add_path -a $HOME/.cargo/bin
 fish_add_path -a $HOME/.local/bin
-
-# nix
-fish_add_path /run/wrappers/bin
-fish_add_path /etc/profiles/per-user/$USER/bin
-fish_add_path /run/current-system/sw/bin
-fish_add_path /nix/var/nix/profiles/default/bin
-fish_add_path -a /nix/profile/bin
-fish_add_path -a $HOME/.nix-profile/bin

17129aef7fd6b1f845c44a050bd2f18e040dbc5b
sebastiano <sebastiano.gigliobianco@maxqda.de>
2026-08-22 20:55:03 +0200

    check if programmes exist before using

--- config/fish/conf.d/alias.fish
+++ config/fish/conf.d/alias.fish
@@ -1,5 +1,10 @@
-alias ls="lsd"
-alias cat="bat"
+if command -q lsd
+    alias ls="lsd"
+end
+
+if command -q bat
+    alias cat="bat"
+end
 if set -q TOOLBOX_PATH
     alias podman="flatpak-spawn --host podman"
 end
